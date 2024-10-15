5f1949460473ebe1227055c1c1e7d0802d045c33
Enzo Doguereau <edoguereau@yahoo.fr>
2024-10-15 17:29:04 -0400

    refactor code style

--- src/components/TimeLine/TimeLine.tsx
+++ src/components/TimeLine/TimeLine.tsx
@@ -1,3 +1,12 @@
+import TimelineOppositeContent, {
+  timelineOppositeContentClasses,
+} from "@mui/lab/TimelineOppositeContent";
+
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import Image from "../../library/Image.tsx";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import React from "react";
 import Timeline from "@mui/lab/Timeline";
@@ -5,30 +14,24 @@ import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineOppositeContent, {
-  timelineOppositeContentClasses,
-}  from "@mui/lab/TimelineOppositeContent";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import WorkIcon from "@mui/icons-material/Work";
-import { List, ListItem, Typography } from "@mui/material";
-import { Divider } from "@mui/material";
-import Image from "../../library/Image.tsx";
 
 const TimeLine = () => {
   return (
     <>
-      <Timeline sx={{
-        [`& .${timelineOppositeContentClasses.root}`]: {
-          flex: 0.3,
-        },
-      }}>
+      <Timeline
+        sx={{
+          [`& .${timelineOppositeContentClasses.root}`]: {
+            flex: 0.3,
+          },
+        }}
+      >
         <TimelineItem>
           <TimelineOppositeContent color="text.secondary">
-            <Box display="flex" alignItems="flex-end" flexDirection="column" >
-              <Typography>
-                2023
-              </Typography>
+            <Box display="flex" alignItems="flex-end" flexDirection="column">
+              <Typography>2023</Typography>
             </Box>
             <Divider />
           </TimelineOppositeContent>
@@ -42,16 +45,10 @@ const TimeLine = () => {
         </TimelineItem>
         <TimelineItem>
           <TimelineOppositeContent color="text.secondary">
-            <Box display="flex" alignItems="flex-end" flexDirection="column" >
-              <Typography>
-                2023
-              </Typography>
-              <Typography>
-                2022
-              </Typography>
-              <Typography>
-                2021
-              </Typography>
+            <Box display="flex" alignItems="flex-end" flexDirection="column">
+              <Typography>2023</Typography>
+              <Typography>2022</Typography>
+              <Typography>2021</Typography>
             </Box>
             <Divider />
           </TimelineOppositeContent>
@@ -78,10 +75,12 @@ const TimeLine = () => {
         <TimelineItem>
           <TimelineOppositeContent color="text.secondary">
             <Box display="flex" flexDirection="column" alignItems="center">
-              <Image src="./images/logo_epitech.png" height="3em" width="2.5em" />
-              <Typography>
-                Epitech
-              </Typography>
+              <Image
+                src="./images/logo_epitech.png"
+                height="3em"
+                width="2.5em"
+              />
+              <Typography>Epitech</Typography>
             </Box>
             <Divider />
           </TimelineOppositeContent>
@@ -93,19 +92,13 @@ const TimeLine = () => {
           <TimelineContent display="flex">
             <List>
               <ListItem>
-                <Typography>
-                  2020 - Bachelor, Troisième année
-                </Typography>
+                <Typography>2020 - Bachelor, Troisième année</Typography>
               </ListItem>
               <ListItem>
-                <Typography>
-                  2019 - Bachelor, Seconde année
-                </Typography>
+                <Typography>2019 - Bachelor, Seconde année</Typography>
               </ListItem>
               <ListItem>
-                <Typography>
-                  2018 - Bachelor, Première année
-                </Typography>
+                <Typography>2018 - Bachelor, Première année</Typography>
               </ListItem>
             </List>
           </TimelineContent>
