080d101e879c20ec8bcc9cb84247d50a02406c9d
Enzo Doguereau <edoguereau@yahoo.fr>
2024-11-12 23:13:56 -0500

    update of the Banner, and skillset graph

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,22 +1,21 @@
 import { Box, styled, useTheme } from "@mui/material";
 
-import { BarChart } from '@mui/x-charts/BarChart';
+import { BarChart } from "@mui/x-charts/BarChart";
 import Image from "../library/Image.tsx";
 import React from "react";
 
 const Banner = () => {
   const theme = useTheme();
 
-  const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-  const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+  const uData = [1, 2, 3, 4, 2, 1, 0];
   const xLabels = [
-    'Page A',
-    'Page B',
-    'Page C',
-    'Page D',
-    'Page E',
-    'Page F',
-    'Page G',
+    "C",
+    "C++",
+    "Python",
+    "Java",
+    "JavaScript/TypeScript",
+    "Kotlin",
+    "C#",
   ];
 
   return (
@@ -31,21 +30,24 @@ const Banner = () => {
         <BarChart
           width={1000}
           height={500}
-          series={[
-            { data: pData, label: 'pv', id: 'pvId' },
-            { data: uData, label: 'uv', id: 'uvId' },
+          series={[{ data: uData, label: "uv", id: "uvId" }]}
+          xAxis={[{ data: xLabels, scaleType: "band" }]}
+          yAxis={[
+            {
+              valueFormatter: (value) => ["inconnu", "oublié", "bon"][value],
+              tickInterval: [0, 1, 2, 3, 4, 5],
+            },
           ]}
-          xAxis={[{ data: xLabels, scaleType: 'band' }]}
         />
       </Box>
     </StyledBox>
   );
 };
 
-const StyledBox = styled(Box)(({theme}) => ({
+const StyledBox = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "row",
-  backgroundColor: "#DFF2EB"
-}))
+  backgroundColor: "#DFF2EB",
+}));
 
 export default Banner;
