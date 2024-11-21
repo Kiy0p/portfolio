9acaf0ea30d811f76018058490ffa2741d38af93
Enzo Doguereau <edoguereau@yahoo.fr>
2024-11-20 21:57:23 -0500

    update barcharts

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,16 +7,26 @@ import React from "react";
 const Banner = () => {
   const theme = useTheme();
 
-  const uData = [4, 3, 1, 4, 4, 3, 0];
-  const xLabels = [
-    "C",
+  const languagesData = [5, 5, 4, 3, 3, 3, 1, 0];
+  const languagesLabels = [
     "C++",
-    "Python",
     "Java",
+    "C",
     "JavaScript/TypeScript",
     "Kotlin",
+    "Python",
     "C#",
-    "Haskell"
+    "Haskell",
+    "Assembly"
+  ];
+
+  const frameworksData = [3, 3, 2, 1, 0];
+  const frameworksLabels = [
+    "React",
+    "Spring",
+    "Vue",
+    "Unity",
+    "Jupiter",
   ];
 
   return (
@@ -27,26 +37,52 @@ const Banner = () => {
         height={theme.spacing(135)}
         sx={{ ml: theme.spacing(10) }}
       />
-      <StyledBox>
-        <Stack direction="column-reverse" spacing={4}>
-          <StyledPaper>Utilisé un jour... mais oublié.</StyledPaper>
-          <StyledPaper>Syntaxe et types: ça commence à sonner familier !</StyledPaper>
-          <StyledPaper>Héritage et librairies: je fais des liens !</StyledPaper>
-          <StyledPaper>Code avec aisance: je nage dans les concepts !</StyledPaper>
-          <StyledPaper>Pas besoin de ChatGPT ou de lead dev</StyledPaper>
-        </Stack>
-        <BarChart
-          width={1000}
-          height={500}
-          series={[{ data: uData, id: "uvId" }]}
-          xAxis={[{ data: xLabels, scaleType: "band" }]}
-          yAxis={[
-            {
-              tickInterval: [0, 1, 2, 3, 4, 5],
-            },
-          ]}
-        />
-      </StyledBox>
+      <Box>
+        <StyledBox>
+          <Stack direction="column-reverse" spacing={4}>
+            <StyledPaper>Utilisé un jour... mais oublié.</StyledPaper>
+            <StyledPaper>Syntaxe et types: ça commence à sonner familier !</StyledPaper>
+            <StyledPaper>Héritage et librairies: je fais des liens !</StyledPaper>
+            <StyledPaper>Code avec aisance: je nage dans les concepts !</StyledPaper>
+            <StyledPaper>Pro: Pas besoin de ChatGPT ou de lead dev</StyledPaper>
+          </Stack>
+          <BarChart
+            width={1000}
+            height={500}
+            series={[{ data: languagesData, id: "languageId" }]}
+            xAxis={[{ data: languagesLabels, scaleType: "band" }]}
+            yAxis={[
+              {
+                min: 0,
+                max: 5,
+                tickInterval: [0, 1, 2, 3, 4, 5],
+              },
+            ]}
+          />
+        </StyledBox>
+        <StyledBox>
+          <Stack direction="column-reverse" spacing={4}>
+            <StyledPaper>Utilisé un jour... mais oublié.</StyledPaper>
+            <StyledPaper>Syntaxe et types: ça commence à sonner familier !</StyledPaper>
+            <StyledPaper>Héritage et librairies: je fais des liens !</StyledPaper>
+            <StyledPaper>Code avec aisance: je nage dans les concepts !</StyledPaper>
+            <StyledPaper>Pro: Pas besoin de ChatGPT ou de lead dev</StyledPaper>
+          </Stack>
+          <BarChart
+            width={1000}
+            height={500}
+            series={[{ data: frameworksData, id: "frameworkId" }]}
+            xAxis={[{ data: frameworksLabels, scaleType: "band" }]}
+            yAxis={[
+              {
+                min: 0,
+                max: 5,
+                tickInterval: [0, 1, 2, 3, 4, 5]
+              },
+            ]}
+          />
+        </StyledBox>
+      </Box>
     </StyledContainer>
   );
 };
@@ -65,7 +101,6 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
 const StyledBox = styled(Box)(({ theme })=> ({
   display: "flex",
   alignItems: "center",
-  height: theme.spacing(100)
 }));
 
 const StyledContainer = styled(Box)(({ theme }) => ({
